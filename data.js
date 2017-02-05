3a77709e3c58d0dcc942b191e17306f6abd92a59
ericzheng <js-class@qq.com>
2017-02-06 01:25:19 +0800

    a

---
+++ data.js
@@ -0,0 +1,20 @@
+var data = [
+    {
+        name: 'iPhone 6',
+        price: 5499,
+        id: 1,
+        count: 1
+    },
+    {
+        name: 'iPad',
+        price: 3299,
+        id: 2,
+        count: 1
+    },
+    {
+        name: 'iMac',
+        price: 15999,
+        id: 3,
+        count: 1
+    }
+];
